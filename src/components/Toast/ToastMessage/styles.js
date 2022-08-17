4ea3a5f2437e866331d195571f4b79846b0180fb
luan AC <luanalbucan@hotmail.com>
2022-08-17 16:25:41 -0300

    variants

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,14 +1,28 @@
 import styled, { css } from 'styled-components';
 
+const containerVariants = {
+  default: css`
+  background-color: ${({ theme }) => theme.colors.primary.main};
+  `,
+  success: css`
+  background-color: ${({ theme }) => theme.colors.success.main};
+  `,
+  danger: css`
+  background-color: ${({ theme }) => theme.colors.danger.main};
+  `,
+};
+
 export const Container = styled.div`
     padding: 16px 32px;
-    background-color: ${({ theme }) => theme.colors.primary.main};
+
     color: #fff;
     border-radius: 4px;
     box-shadow: 0px 20px 20px -16px rgba(0, 0, 0, 0.25);
     & + & {
         margin-top: 12px;
     }
+
+    ${({ type }) => containerVariants[type] || containerVariants.default}
 
     display: flex;
     align-items: center;
@@ -18,9 +32,5 @@ export const Container = styled.div`
         margin-right: 8px;
     }
 
-    ${({ type }) => type !== 'default'
-    && css`
-    background-color: ${({ theme }) => theme.colors[type].main};
-    `
-}
+
 `;
